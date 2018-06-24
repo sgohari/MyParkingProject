2a8e827dbc497b42f7c24fab6069bcaeb6a2c8ee
sgohary <nasir_2005@msn.com>
2018-06-24 19:12:31 -0400

    Two Buttons added for home page

--- app/src/main/java/com/example/nasir/myparking/CustomerHomePage.java
+++ app/src/main/java/com/example/nasir/myparking/CustomerHomePage.java
@@ -9,5 +9,7 @@ public class CustomerHomePage extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_home_page);
+
+
     }
 }
