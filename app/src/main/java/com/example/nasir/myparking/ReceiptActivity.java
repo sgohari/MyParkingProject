587ed8c081f7d890e82d40fe882def5b96750615
sgohary <nasir_2005@msn.com>
2018-07-06 18:43:01 -0400

    All functions are tested, changes are made.Final tunning is required

--- app/src/main/java/com/example/nasir/myparking/ReceiptActivity.java
+++ app/src/main/java/com/example/nasir/myparking/ReceiptActivity.java
@@ -1,5 +1,6 @@
 package com.example.nasir.myparking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -16,10 +17,18 @@ public class ReceiptActivity extends AppCompatActivity {
 
         Random random= new Random();
 
-        int number=random.nextInt(10000)+1000000;
-
-        TextView display=(TextView)findViewById(R.id.displayTxt);
+        int number=random.nextInt(1000000000)+1000000000;
         String myString=String.valueOf(number);
-        display.setText("Customer Name: "+"\n\n"+"Receipt ID"+"\t\t\t\t"+myString+"\n\n"+"Parking Lot Name:"+"\n\n"+"Parking Lot Address:");
+
+
+        Intent getMessage = getIntent();
+        String name= getMessage.getStringExtra("Name");
+        String parkingLotName=getMessage.getStringExtra("parkingLotName");
+        String parkingAddress=getMessage.getStringExtra("parkingAddress");
+        TextView display=(TextView)findViewById(R.id.displayTxt);
+
+        display.setText("Customer Name: "+"\t\t\t\t\t\t"+name+"\n\n"+"Receipt ID"+"\t\t\t\t\t\t\t\t\t\t\t\t\t"+
+                myString+"\n\n"+"Parking Lot Name:"+"\t\t\t\t"+parkingLotName+
+                "\n\n"+"Parking Lot Address:"+"\t\t\t"+parkingAddress);
     }
 }
--- app/src/main/java/com/example/nasir/myparking/ReservationsActivity.java
+++ app/src/main/java/com/example/nasir/myparking/ReservationsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class ReservationsActivity extends AppCompatActivity {
 
@@ -16,6 +17,17 @@ public class ReservationsActivity extends AppCompatActivity {
 
     public void confirm_Onclick (View view) {
 
-        startActivity(new Intent(ReservationsActivity.this,ReceiptActivity.class));
+        EditText custNameET= (EditText)findViewById(R.id.customerNameET);
+        EditText pkLotName=(EditText)findViewById(R.id.parkingNameET);
+        EditText pkAddress=(EditText)findViewById(R.id.parkingLotAddressET);
+
+        Intent intentNext = new Intent(ReservationsActivity.this,ReceiptActivity.class);
+
+        intentNext.putExtra("Name",custNameET.getText().toString());
+        intentNext.putExtra("parkingLotName",pkLotName.getText().toString());
+        intentNext.putExtra("parkingAddress",pkAddress.getText().toString());
+
+        startActivity(intentNext);
+
     }
 }
