a58149d3eeb48034730c359967ec51bb6ad183d2
Luis Sanchez <lsanch14@my.centennialcollege.ca>
2018-08-12 19:13:28 -0400

    Database classes and DBHelper

---
+++ app/src/main/java/com/example/nasir/myparking/Database/registrationTable.java
@@ -0,0 +1,19 @@
+package com.example.nasir.myparking.Database;
+
+public class registrationTable {
+
+    //Registration table fields
+    public static final String PK_USER_ID = "userID"; //PK
+    public static final String FK_RESERVATION_ID = "reservationID"; //FK
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_FIRST_NAME = "firstName";
+    public static final String KEY_LAST_NAME = "lastName";
+    public static final String KEY_ADDRESS = "address";
+    public static final String KEY_CITY= "city";
+    public static final String KEY_POSTAL_CODE = "postalCode";
+
+    //TARGET
+    public static final String DATABASE_CREATE_USERS =
+            "create table if not exists registration (userID integer primary key , password text not null, " //username is int
+                    + "firstName text not null, lastName text not null, address text, city text not null, postalCode text);";
+}
---
+++ app/src/main/java/com/example/nasir/myparking/Database/reservationTable.java
@@ -0,0 +1,21 @@
+package com.example.nasir.myparking.Database;
+
+public class reservationTable {
+    //Reservation table fields
+    public static final String PK_RESERVATION_ID = "reservationID"; //PK
+    public static final String FK_USER_ID = "userID"; //FK
+    public static final String KEY_NAME = "userName";
+    public static final String KEY_PARKING_NAME = "parkingName";
+    public static final String KEY_PARKING_ADDRESS = "parkingAddress";
+    public static final String KEY_TIME_FROM = "timeFrom";
+    public static final String KEY_TIME_TO = "timeTo";
+    public static final String KEY_CARD_NUMBER = "cardNumber";
+    public static final String KEY_EXPIRY_DATE = "expiryDate";
+    public static final String KEY_CVV = "cvv";
+
+    //TARGET
+    public static final String DATABASE_CREATE_RESERVATIONS =
+            "create table if not exists reservation (reservationID integer primary key autoincrement,userID integer, userName text not null, " //username is int
+                    + "parkingName text not null, parkingAddress text not null, timeFrom text, timeTo text not null, cardNumber integer not null, expiryDate text" +
+                    ",cvv integer not null,foreign key(userID) references registration(userID));"; //missing FK
+}
