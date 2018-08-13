335955eeb30bbe2400301cff448102ef214ce449
sgohary <nasir_2005@msn.com>
2018-08-13 01:09:53 -0400

    Reservation bugs fixed

--- app/src/main/java/com/example/nasir/myparking/ReservationsActivity.java
+++ app/src/main/java/com/example/nasir/myparking/ReservationsActivity.java
@@ -43,18 +43,18 @@ public class ReservationsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservations);
 
-        myDb=new DataSource(this);
+        myDb = new DataSource(this);
 
         //get Intent extra (username)
         username = getIntent().getStringExtra("username");
 
-        parkingNameET=(EditText)findViewById(R.id.parkingNameET);
-        parkingAddressET=(EditText)findViewById(R.id.parkingLotAddressET);
-        timeFromET=(EditText)findViewById(R.id.fromET);
-        timeToET=(EditText)findViewById(R.id.toET);
-        cardNumberET=(EditText)findViewById(R.id.cardNumberET);
-        expiryDateET=(EditText)findViewById(R.id.expireDateET);
-        cvvET=(EditText)findViewById(R.id.securityCodeET);
+        parkingNameET = (EditText) findViewById(R.id.parkingNameET);
+        parkingAddressET = (EditText) findViewById(R.id.parkingLotAddressET);
+        timeFromET = (EditText) findViewById(R.id.fromET);
+        timeToET = (EditText) findViewById(R.id.toET);
+        cardNumberET = (EditText) findViewById(R.id.cardNumberET);
+        expiryDateET = (EditText) findViewById(R.id.expireDateET);
+        cvvET = (EditText) findViewById(R.id.securityCodeET);
 
         //Assign Values
         parkingName = parkingNameET.getText().toString();
@@ -65,166 +65,34 @@ public class ReservationsActivity extends AppCompatActivity {
         expiryDate = expiryDateET.getText().toString();
         cvv = cvvET.getText().toString();
 
+        displaySharedInfor();
         //Radio Group
-        rdGroup=(RadioGroup)findViewById(R.id.rdgGenders);
+        rdGroup = (RadioGroup) findViewById(R.id.rdgGenders);
 
         //Assigning card type
         rdGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int i) {
-                RadioButton checkedRadioButton = (RadioButton)group.findViewById(i);
+            public void onCheckedChanged (RadioGroup group, int i) {
+                RadioButton checkedRadioButton = (RadioButton) group.findViewById(i);
                 // This puts the value (true/false) into the variable
                 boolean isChecked = checkedRadioButton.isChecked();
                 // If the radiobutton that has changed in check state is now checked...
-                if (isChecked)
-                {
+                if (isChecked) {
                     // Changes the textview's text to "Checked: example radiobutton text"
-                   cardType = checkedRadioButton.getText().toString();
+                    cardType = checkedRadioButton.getText().toString();
                 }
             }
         });
-
-
-        //insertingData();
-
     }
 
-   /* public void insertingData(){
+    public void displaySharedInfor(){
+        SharedPreferences sharedPreferences=getSharedPreferences("markerContent", Context.MODE_PRIVATE);
+        String title = sharedPreferences.getString("title","");
+        String snipped=sharedPreferences.getString("snipped","");
+        parkingNameET.setText(title);
+        parkingAddressET.setText(snipped);
 
-        btnSaves.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v) {
-                Intent intentNext = new Intent(ReservationsActivity.this, ReceiptActivity.class);
-                intentNext.putExtra("Name", custNameET.getText().toString());
-                intentNext.putExtra("parkingLotName", pkLotName.getText().toString());
-                intentNext.putExtra("parkingAddress", pkAddress.getText().toString());
-
-                getName = custNameET.getText().toString();
-                getPkLotName=pkLotName.getText().toString();
-                getkAddress=pkAddress.getText().toString();
-                getFrom=timeFrom.getText().toString();
-                getTo=timeTo.getText().toString();
-                getCardNumber=cardNumber.getText().toString();
-                getExpireDate=expDate.getText().toString();
-                getSecurity=securityCode.getText().toString();
-
-                try
-                {
-
-
-                   //Set Time Format
-                    DateFormat fmt = new SimpleDateFormat("HH:mm");
-
-                    //Set Expired Date Format
-                    DateFormat mmyy = new SimpleDateFormat("MM/yyyy");
-                    DateFormat yy = new SimpleDateFormat("yyyy");
-                    DateFormat mm = new SimpleDateFormat("MM");
-                    mmyy.setLenient(false);
-
-                    //Setting Current year, month
-                    Calendar calendar = new GregorianCalendar();
-                    int currentYear = calendar.get(Calendar.YEAR);
-                    int currentMonth  = calendar.get(Calendar.MONTH) + 1;
-
-                    //Full Result of expired Date
-                    result = mmyy.parse(getExpireDate); // returns the full date month year day etc
-
-                    //User inputed Year, month
-                    String year = yy.format(result);
-                    String month = mm.format(result);
-                    // Full Result of time
-                    RTimeFrom = fmt.parse(getFrom);
-                    RTimeTo = fmt.parse(getTo);
-
-                    //Checkers
-                    Boolean yearDateCheck = false;
-                    Boolean timeCheck = false;
-
-                    if(RTimeTo.after(RTimeFrom))
-                    {
-                        timeCheck = true;
-
-                    }
-                    else
-                        {
-                            Toast.makeText(ReservationsActivity.this,"Enter a valid booking time ",Toast.LENGTH_LONG).show();
-
-                        }
-
-                    if (Integer.parseInt(year) <= currentYear ){
-
-                    if (currentYear == Integer.parseInt(year) && yearDateCheck == false){
-                        if (currentMonth + 1 > Integer.parseInt(month)){
-                            Toast.makeText(ReservationsActivity.this,"Date must be in 'MM/yyyy and a month greater than " + currentMonth  ,Toast.LENGTH_LONG).show();
-                            yearDateCheck = false;
-                        }
-                        else
-                            {
-                                yearDateCheck = true;
-                            }
-
-                    }
-                    else if (Integer.parseInt(year) < currentYear && yearDateCheck && yearDateCheck )
-                    {
-                        yearDateCheck = false;
-                        Toast.makeText(ReservationsActivity.this,"Date must be in 'MM/yyyy and greater than year " + year ,Toast.LENGTH_LONG).show();
-                    }
-
-
-                }
-                else
-                    {
-                        yearDateCheck = true;
-                    }
-
-                    if (getName.equals("")|| getPkLotName.equals("")|| getkAddress.equals("")|| getFrom.equals("")|| getTo.equals("") || getCardNumber.equals("")|| getExpireDate.equals("")||getSecurity.equals("")
-                         || yearDateCheck == false || timeCheck == false)
-                {
-
-
-                    if (cardNumber.getText().toString().length()>16){
-                        Toast.makeText(ReservationsActivity.this,"card number Must be 16 digits",Toast.LENGTH_LONG).show();
-
-                    }
-
-
-                    else if (securityCode.getText().toString().length()>3){
-                        Toast.makeText(ReservationsActivity.this,"security code must be 3 digits ",Toast.LENGTH_LONG).show();
-
-                    }
-
-
-                    Toast.makeText(ReservationsActivity.this,"Fields are Required",Toast.LENGTH_LONG).show();
-                    expDate.setText(" ");
-                    expDate.setHint("MM/YYYY");
-                    timeFrom.setText("00:00");
-                    timeTo.setText("00:00");
-
-
-
-                    return;
-                }
-                    boolean isInserted = myDb.insertData(custNameET.getText().toString(),pkLotName.getText().toString(),pkAddress.getText().toString(),RTimeFrom.toString(),RTimeTo.toString(), cardNumber.getText().toString(),result.toString(),securityCode.getText().toString());
-                    if (isInserted==true){
-
-                        startActivity(intentNext);
-
-                        Toast.makeText(ReservationsActivity.this,"Record Added to DB",Toast.LENGTH_LONG).show();
-
-                    }else {
-                        Toast.makeText(ReservationsActivity.this,"Record is not added to DB",Toast.LENGTH_LONG).show();
-                    }
-
-                }catch(Exception e)
-                {
-                    Toast.makeText(ReservationsActivity.this,"Something Went Absolutely Wrong, " +
-                            "please follow the hints and make sure all fields are filled!!",Toast.LENGTH_LONG).show();
-                } }
-
-        }
-        );
-    }*/
-
+    }
     //Clear all fields
     public void clear_onClick(View view){
         btnClears.setOnClickListener(new View.OnClickListener() {
